2c6e0f2d5d55590e842d2de5c76562720c929c15
Max <leonovv-max@yandex.ru>
2025-08-16 16:33:31 +0300

    нахождение решений ур-ния

--- main.c
+++ main.c
@@ -2,21 +2,17 @@
 #include <math.h>
 #include <string.h>
 #include <stdlib.h>
-#define MAXSIZE 100
 
 int main(){
-    int a, b, c;
-    int ind = 0, len = 0, arg[3];
-    char symb, str[MAXSIZE];
-    
-    // scanf("%s",str);
-    // len = strlen(str);
-    // printf("%s",str);
+    int ind = 0, arg[3]; //вспомогательная переменная и массив для хранения коэффициентов
+    float x1, x2; //переменные для решений уравнения
+    char symb, str[100]; //переменная и массив для ввода коэффициентов
 
-    while(1){
+    printf("Enter coefficients separated by space\n");
+    
+    while(1){ //цикл ввода коэффициентов
         scanf("%s",str);
-        arg[ind] = strtol(str,NULL,10);
-        printf("%ld\n",arg[ind]); 
+        arg[ind] = strtol(str,NULL,10); 
         ind++;
 
         if(ind >= 3){     
@@ -24,5 +20,26 @@ int main(){
         }
     }
 
+    int Discr(int arg[]){ //расчет дискриминанта
+        return arg[1]*arg[1] - 4*arg[0]*arg[2];
+    }
+
+    if(Discr(arg) > 0){ //решения при дискриминанте >0
+
+        x1 = (-arg[1] + sqrt(Discr(arg)))/(2*arg[0]);
+        x2 = (-arg[1] - sqrt(Discr(arg)))/(2*arg[0]);
+        printf("Solutions: %.2f %.2f \n",x1,x2);
+
+    } else if (Discr(arg) == 0){ //решения при дискриминанте =0
+
+        x1 = -arg[1]/(2*arg[0]);
+        printf("Solution: %.2f \n",x1);
+
+    } else{ //случай отсутствия решений
+
+        printf("There is no rational solutions");
+
+    }
+    
     return 0;
 }
