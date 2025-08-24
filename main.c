265296f961697c8cb047b468cdb99040ccb43605
Max <leonovv-max@yandex.ru>
2025-08-24 14:08:00 +0300

    sdffdsf

--- main.c
+++ main.c
@@ -5,64 +5,77 @@
 
 
 
-int     DoubleCompare (double double_1, double double_2);
-int     SolveSquare   (double* coefficient_a, double* coefficient_b, double* coefficient_c,
-                       double* solution_1, double* solution_2);
-int     SolveLinear   (double* coefficient_b, double* coefficient_c,
-                       double* solution_1, double* solution_2);
-double* ScanSolutions (double coefficients[]);
-void    PrintSolutions(int solution_number, double* solution_1, double* solution_2);
+struct coefficients {
+    double a, b, c;
+};
+
+struct coefficients equation_coeff = { .a = 0, .b = 0, .c = 0 };
+
+struct result {
+    double x1, x2;
+};
+
+struct result equation_res = { .x1 = 0, .x2 = 0 };
+
+
+
+
+int     DoubleCompare    (double double_1, double double_2);
+int     SolveSquare      (double* coefficient_a, double* coefficient_b, double* coefficient_c,
+                          double* solution_1, double* solution_2);
+int     SolveLinear      (double* coefficient_b, double* coefficient_c,
+                          double* solution_1, double* solution_2);
+void    ScanCoefficients (struct coefficients equation_coeff);
+void    PrintSolutions   (int solution_number, double* solution_1, double* solution_2);
+void    ResetInputBuffer (void);
 
 
 
 int main() {
+    
+    int solution_number = 0;
 
-    const int coefficients_number = 3;
-    int       solution_number = 0;
-    double    coefficients[coefficients_number] = {0};
-    double    solution_1 = 0, solution_2 = 0;                   
+    ScanCoefficients(equation_coeff);
 
-    ScanSolutions(coefficients);
-
-    if(DoubleCompare(coefficients[0], 0)) {
-        solution_number = SolveLinear(&coefficients[1], &coefficients[2], &solution_1, &solution_2);
+    if (DoubleCompare(equation_coeff.a, 0)) {
+        solution_number = SolveLinear(&equation_coeff.b, &equation_coeff.c,
+                                      &equation_res.x1, &equation_res.x2);
     }
     else {
-        solution_number = SolveSquare(&coefficients[0], &coefficients[1], &coefficients[2], &solution_1, &solution_2);
+        solution_number = SolveSquare(&equation_coeff.a, &equation_coeff.b, &equation_coeff.c,
+                                      &equation_res.x1, &equation_res.x2);
     }
 
-    PrintSolutions(solution_number, &solution_1, &solution_2);
+    PrintSolutions(solution_number, &equation_res.x1, &equation_res.x2);
     
     return 0;
 }
 
 
 
-double* ScanSolutions(double coefficients[]) {
+void ScanCoefficients(struct coefficients equation_coeff) {
     int coefficient_number = 0;
 
-    while(1){
+    while(1) {
         printf("Enter coefficients separated by space\n");
-        coefficient_number = scanf( "%lf %lf %lf", &coefficients[0], &coefficients[1], &coefficients[2]);
+        coefficient_number = scanf( "%lf %lf %lf", &equation_coeff.a, &equation_coeff.b, &equation_coeff.c);
 
         if(coefficient_number < 3) {
-            while(getchar() != '\n');
+            ResetInputBuffer();
             coefficient_number = 0;
 
             printf("Incorrect input, try again.\n");
-        }
+        } 
         else {
             break;
         }
     }
-
-    return coefficients;
 }
 
 
 
-int SolveLinear   (double* coefficient_b, double* coefficient_c,
-                   double* solution_1, double* solution_2) {
+int SolveLinear(double* coefficient_b, double* coefficient_c,
+                double* solution_1, double* solution_2) {
 
     if (DoubleCompare(*coefficient_b, 0)) {
 
@@ -86,7 +99,7 @@ int SolveSquare(double* coefficient_a, double* coefficient_b, double* coefficien
 
     double Discriminant = (*coefficient_b)*(*coefficient_b) - 4*(*coefficient_a)*(*coefficient_c);
 
-    if(Discriminant < 0) {
+    if (Discriminant < 0) {
         return 0;
     }
     else {
@@ -102,19 +115,19 @@ int SolveSquare(double* coefficient_a, double* coefficient_b, double* coefficien
 void PrintSolutions(int solution_number, double* solution_1, double* solution_2) {
 
     switch(solution_number) {
-        case -1 :
+        case -1:
             printf("Infinite number of solutions");
             break;
 
-        case 0 :
+        case 0:
             printf("There is no solutions");
             break;
 
-        case 1 :
+        case 1:
             printf("It's a linear equation. Solution is %.2lf", *solution_1);
             break;
 
-        case 2 :
+        case 2:
             if (DoubleCompare(*solution_1, *solution_2)) {
                 printf("Solution is: %.2lf", *solution_1);
             }
@@ -136,4 +149,9 @@ int DoubleCompare(double double_1, double double_2) {
     const double error = 1e-6;
 
     return (fabs(double_1 - double_2) < error) ? 1 : 0;
+}
+
+void ResetInputBuffer(void) {
+
+    while(getchar() != '\n');
 }
